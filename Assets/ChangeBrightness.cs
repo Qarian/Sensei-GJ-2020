340073779e04eb30931c517e195ef0513ffa75f4
Toster <tostermocy@gmail.com>
2020-01-25 11:07:35 +0100

    jeszcze zmiany przed spankiem

---
+++ Assets/ChangeBrightness.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeBrightness : MonoBehaviour
+{
+    private Image _spriteRenderer;
+    private Color _color;
+
+    public float alpha;
+
+    private void Start()
+    {
+        _spriteRenderer = gameObject.GetComponent<Image>();
+        _color = _spriteRenderer.color;
+        alpha = _color.a;
+    }
+
+    private void Update()
+    {
+        ChangeAlpha();
+    }
+
+    void ChangeAlpha()
+    {
+        _color.a = alpha;
+    }
+}
