a21499c224383be0d9052d55f01fc0e07504b9ee
PRIDEMASTER <michal.winkler.22.10@gmail.com>
2020-01-25 10:56:58 +0100

    Added DI

---
+++ Assets/Scripts/DI/DI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SenseiGameJam.DependencyInjection
+{
+    public static class DI
+    {
+        static Dictionary<System.Type, IDependency> singletons;
+        static readonly EmptyDependency emptyDependency = new EmptyDependency();
+
+        public static IDependency GetSingleton(System.Type type)
+        {
+            singletons.TryGetValue(type, out IDependency result);
+            if(result != null)
+            {
+                return result;
+            }
+            else
+            {
+                return emptyDependency;
+            }
+        }
+    }
+}
