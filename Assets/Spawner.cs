e25d92251a0d527e98d3af81b9a728e86b695156
Toster <tostermocy@gmail.com>
2020-01-25 13:31:41 +0100

    bouncy kotki
    skrypt skaczacych bouncy koteczkow

---
+++ Assets/Spawner.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private int numberOfKitties = 10;
+
+    public GameObject kitties;
+
+    private Transform _spawnerTransform;
+
+    private void Start()
+    {
+        _spawnerTransform = transform;
+        for (int i = 1; i <= numberOfKitties; i++)
+        {
+            Vector3 kittyPosition = _spawnerTransform.position;
+            kittyPosition.x += Random.Range(-10, 11);
+            kittyPosition.y += Random.Range(-5, 5);
+            Transform kittyTransform =  Instantiate(kitties, _spawnerTransform).transform;
+            kittyTransform.position = kittyPosition; 
+        }
+    }
+    
+}
