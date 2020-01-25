3866b493f3dfc6dee95d296a7de1605db2d27f9b
Unknown <jakubeklech@o2.pl>
2020-01-25 22:52:01 +0100

    added MORE art

---
+++ Assets/SCRYPT.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using S = UnityEngine.SerializeField;
+
+public class SCRYPT : MonoBehaviour
+{
+    [S] Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+         transform.position =  new Vector3( transform.position.x, transform.position.y, 0);
+
+         if(Input.GetKeyDown(KeyCode.Mouse0))
+         {
+             animator.SetTrigger("trigger");
+         }
+    }
+}
